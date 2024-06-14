a1566e62ed18af6bf7579ff98d17c10f7b63ff10
Julian Osorio <jaosoriom27@gmail.com>
2024-06-14 11:27:29 -0500

    Creación de la entidad, datasources y repositories

---
+++ lib/domain/repositories/movies_repository.dart
@@ -0,0 +1,13 @@
+// Aqui llamamos al datasource
+
+import 'package:cinemapedia/domain/entities/movie.dart';
+
+// Se define como luce los origines de datos, metodos que se usaran para llamar la data
+abstract class MovieRepository {
+
+
+// Reguresa las peliculas que estan actulamente en cartelera --- Solo implementamos
+// Será una lista que a futuro me regrese una movi, por parametro será siempre paginado 
+Future<List<Movie>> getNowPlaying ( { int page = 1 } );
+
+}
