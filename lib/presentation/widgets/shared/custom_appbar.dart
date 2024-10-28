ced2108897d8cdf915a457de219e3b96f5c11192
Julian Osorio <jaosoriom27@gmail.com>
2024-10-28 10:47:21 -0500

    Controlando posibles valores nulos

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,10 +1,13 @@
+import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CustomAppbar extends StatelessWidget {
+class CustomAppbar extends ConsumerWidget {
   const CustomAppbar({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
 
     final colors = Theme.of(context).colorScheme;
     final titleStyle = Theme.of(context).textTheme.titleMedium;
@@ -25,7 +28,18 @@ class CustomAppbar extends StatelessWidget {
               //Envia el proximo elemento al final con el espacio disponible
               const Spacer(),
               IconButton(onPressed: () {
-                
+
+                // Creamos una funcion para hacer referencia 
+                final movieRepository =  ref.read(movieRepositoryProvider);
+
+
+                // Realizamos la busqueda
+                showSearch(
+                  context: context, 
+                  delegate: SearchMovieDelegate(
+                    searchMovies: movieRepository.searchMovies
+                  )
+                );
               },
               icon:const Icon (Icons.search)
               )
